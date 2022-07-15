a2d83ac5cc0f73c3d0fa699491e4c3520fec8eda
MrN1x0n <95374645+MrN1x0n@users.noreply.github.com>
2022-07-15 11:57:37 +0300

    Update massa_install.sh

--- massa_install.sh
+++ massa_install.sh
@@ -1,5 +1,32 @@
-# скачиваем бинарник и распаковываем его
+#!/bin/bash
+exists()
+{
+  command -v "$1" >/dev/null 2>&1
+}
+if exists curl; then
+echo ''
+else
+  sudo apt update && sudo apt install curl -y < "/dev/null"
+fi
+bash_profile=$HOME/.bash_profile
+if [ -f "$bash_profile" ]; then
+    . $HOME/.bash_profile
+fi
+sleep 1 && curl -s https://raw.githubusercontent.com/MrN1x0n/MrN1x0n/main/logo.sh | bash && sleep 1
+
+
+if [ ! $MASSA_PASSWORD ]; then
+read -p "Enter massa wallet password: " MASSA_PASSWORD
+echo 'export MASSA_PASSWORD='\"${MASSA_PASSWORD}\" >> $HOME/.bash_profile
+fi
+echo 'source $HOME/.bashrc' >> $HOME/.bash_profile
+. $HOME/.bash_profile
+sleep 1
 cd $HOME
+sudo apt update && sudo apt upgrade -y
+sudo apt install wget curl jq unzip git build-essential pkg-config libssl-dev -y < "/dev/null"
+
+# скачиваем бинарник и распаковываем его
 
 version=`wget -qO- https://api.github.com/repos/massalabs/massa/releases/latest | jq -r ".tag_name"`; \
 curl -sL "https://github.com/massalabs/massa/releases/download/${version}/massa_${version}_release_linux.tar.gz" > release_linux.tar.gz
